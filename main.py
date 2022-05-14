99ce56ed085d96c681c0b7c11bdf26605ebc0377
tumka <tumka5688g@gmail.com>
2022-05-14 11:57:13 +0300

    исправил

--- main.py
+++ main.py
@@ -1,42 +1,33 @@
 import threading
 from threading import Thread
+from datetime import datetime
 from time import sleep
 import random
 
-time_list: list = [0 for _ in range(10)]
-
 
 def useful_work(i):
-    print(f'Выполняется полезная работа... \n Время работы {time_list[i]}, поток {threading.current_thread().name}')
-    sleep(time_list[i])
-    print(f'Полезная работа выполнена. {threading.current_thread().name}')
+    count = 0
+    while True:
+        if count == 0:
+            count = 1
+            time_thread_first = datetime.now()
 
+        print(f'Выполняется полезная работа... \n Время работы {time_list[i]}, поток {threading.current_thread().name}')
+        with open(f'{i}.txt', 'a', encoding='utf-8') as outfile:
+            out_str = f'Время работы {time_list[i]}, поток {threading.current_thread().name} \n'
+            outfile.write(out_str)
 
-list_threads: list = [[random.randint(1, 10), 0, Thread(target=useful_work, args=(i,), name=str(i))] for i in range(10)]
-# возможное исключение list_threads = [[1, 0], [4, 0], [4, 0], [4, 0], [4, 0], [4, 0], [4, 0], [4, 0], [9, 0], [9, 0]]
-list_threads.sort(key=lambda x: x[1])
-print(list_threads)
-min_priority: int = max(map(lambda x: x[0], list_threads))
-
-for i in range(len(list_threads)):
-    buf: list = []
-
-    if list_threads[i][1] != 0:
-        continue
-
-    for j in range(10):
-        if list_threads[i][0] == list_threads[j][0] and list_threads[j][1] == 0:
-            buf.append(j)
-
-    for j in buf:
-        list_threads[j][0] = min_priority
-        list_threads[j][1] = 60 // len(buf)
-
-print(list_threads)
+        sleep(0.5)
+        time_thread_second = datetime.now()
+        time_work = time_thread_second - time_thread_first
+        print(time_work.total_seconds())
+        if float(time_work.total_seconds()) >= time_list[i]:
+            print(f'Полезная работа выполнена. {threading.current_thread().name}')
+            break
 
 
 def processing_data() -> None:
-    global list_threads
+    global list_threads, time_list
     all_time: int = 0
 
     for i in list_threads:
@@ -63,25 +54,61 @@ def processing_data() -> None:
 
     print(list_threads, 'возможно нормальное время работы')
 
+    for i in range(10):
+        list_threads[i][2] = Thread(target=useful_work, args=(i,), name=str(i))
+
+    for i in range(10):
+        time_list[i] = list_threads[i][1]
+
+    for i in range(10):
+        list_threads[i][2].start()
+
+    for i in range(10):
+        list_threads[i][2].join()
+
 
 while True:
-    processing_data()
-    count: int = 0
+    time_list: list = [0 for _ in range(10)]
+    list_threads: list = [[random.randint(1, 10), 0, Thread(target=useful_work, args=(i,),
+                                                            name=str(i))] for i in range(10)]
+    # возможное исключение list_threads = [[1, 0], [4, 0], [4, 0], [4, 0], [4, 0], [4, 0], [4, 0], [4, 0], [9, 0], [9, 0]]
+    list_threads.sort(key=lambda x: x[1])
+    print(list_threads)
+    min_priority: int = max(map(lambda x: x[0], list_threads))
+
+    for i in range(len(list_threads)):
+        buf: list = []
+
+        if list_threads[i][1] != 0:
+            continue
+
+        for j in range(10):
+            if list_threads[i][0] == list_threads[j][0] and list_threads[j][1] == 0:
+                buf.append(j)
+
+        for j in buf:
+            list_threads[j][0] = min_priority
+            list_threads[j][1] = 60 // len(buf)
+
     for i in range(10):
-        if not 5 < list_threads[i][1] < 7:
-            count += 1
+        time_list[i] = list_threads[i][1]
 
-    if count == 0:
-        break
+    for i in range(10):
+        list_threads[i][2].start()
 
-print(list_threads, 'нормальное время')
+    for i in range(10):
+        list_threads[i][2].join()
 
-for i in range(10):
-    time_list[i] = list_threads[i][1]
+    print(list_threads)
 
-for i in range(10):
-    list_threads[i][2].start()
+    while True:
+        processing_data()
+        count: int = 0
+        for i in range(10):
+            if not 5 < list_threads[i][1] < 7:
+                count += 1
 
-for i in range(10):
-    list_threads[i][2].join()
+        if count == 0:
+            break
 
+    print(list_threads, 'нормальное время')
