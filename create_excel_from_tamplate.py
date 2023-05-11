081bd13f79bb2d6aeee4caa631c92a96a8a86213
T1RuS <70951381+T1RuS@users.noreply.github.com>
2023-05-11 17:35:19 +0300

    Update create_excel_from_tamplate.py
    2

--- create_excel_from_tamplate.py
+++ create_excel_from_tamplate.py
@@ -1,4 +1,6 @@
 from xlsxtpl.writerx import BookWriter
+from io import BytesIO
+from openpyxl.writer.excel import save_virtual_workbook
 
 
 def creating_excel_from_template(data, template_path):
@@ -6,6 +8,12 @@ def creating_excel_from_template(data, template_path):
     writer.jinja_env.globals.update(dir=dir, getattr=getattr)
     payload = {'tpl_idx': 1, 'sheet_name': u'Лист1', 'ctx': data}
     writer.render_book2(payloads=[payload])
+    # save_virtual_workbook(writer.workbook) --- создает строку с байтами
+    # BytesIO(save_virtual_workbook(writer.workbook)) --- отправка байтов на фронт клиенту как файл
+
+    with open('test.xlsx', 'wb') as f:
+        f.write(save_virtual_workbook(writer.workbook))
+
     writer.save('resaul.xlsx')
 
 
